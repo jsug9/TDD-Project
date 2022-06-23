fd2a0f253e78bb6926b25714bba952a6a07e719b
Augusto <jesus_augga_99@icloud.com>
2022-06-23 11:24:05 -0500

    add tests to make sure solver has methods and they return the correct element

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -3,15 +3,31 @@ require './solver'
 describe Solver do
   let(:solver) { Solver.new }
   
-  it 'has a factorial method' do
-    expect(solver).to respond_to(:factorial)
+  describe 'has methods' do
+    it 'has a factorial method' do
+      expect(solver).to respond_to(:factorial)
+    end
+
+    it 'has a reverse method' do
+      expect(solver).to respond_to(:reverse)
+    end
+
+    it 'has a fizzbuzz method' do
+      expect(solver).to respond_to(:fizzbuzz)
+    end
   end
 
-  it 'has a reverse method' do
-    expect(solver).to respond_to(:reverse)
-  end
+  describe 'returns a correct value' do
+    it 'factorial returns a number' do
+      expect(solver.factorial(5)).to be_a(Integer)
+    end
 
-  it 'has a fizzbuzz method' do
-    expect(solver).to respond_to(:fizzbuzz)
+    it 'reverse returns a string' do
+      expect(solver.reverse('hello')).to be_a(String)
+    end
+
+    it 'fizzbuzz returns a number' do
+      expect(solver.fizzbuzz(5)).to be_a(Integer)
+    end
   end
 end
