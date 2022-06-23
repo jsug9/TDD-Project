370c1a352217a28e3209fad411a9e82fca18c1ea
fed1k <allamurotovcoder@mail.ru>
2022-06-23 21:21:38 +0500

    add tests to reverse and fizzbuzz methods

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -6,4 +6,12 @@ describe Solver do
   it 'has a factorial method' do
     expect(solver).to respond_to(:factorial)
   end
+
+  it 'has a reverse method' do
+    expect(solver).to respond_to(:reverse)
+  end
+
+  it 'has a fizzbuzz method' do
+    expect(solver).to respond_to(:fizzbuzz)
+  end
 end
