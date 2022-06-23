42f39292cdf97118740bea8fd35ac77325a7546f
Augusto <jesus_augga_99@icloud.com>
2022-06-23 11:19:24 -0500

    add test to check that it has a factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,5 +1,9 @@
-require 'solver'
+require './solver'
 
 describe Solver do
- 
+  let(:solver) { Solver.new }
+  
+  it 'has a factorial method' do
+    expect(solver).to respond_to(:factorial)
+  end
 end
